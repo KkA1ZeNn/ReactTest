cc1af9c4c2ab39803d1118adc6cb0e826c736d84
KkA1ZeN <daniilmanuylov239@gmail.com>
2025-08-02 13:05:53 +0300

    Анимация удаления поста и новый цвет кнопки

--- my-react-app/src/components/Post.component/Post.tsx
+++ my-react-app/src/components/Post.component/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Post.css'
 
 export interface PostProp {
@@ -14,14 +14,29 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = ({ post, deletePost }) => {
+  const [isDeleting, setIsDeleting] = useState(false)
+
+  const handleDelete = () => {
+    setIsDeleting(true)
+    setTimeout(() => {
+      deletePost(post)
+    }, 300) // Время анимации
+  }
+
   return (
-    <li className='post'>
+    <li className={`post ${isDeleting ? 'post--deleting' : ''}`}>
         <div className='post__content'>
             <h2 className='post__title'>{post.arrIndex}. {post.title}</h2>
             <p className='post__text'>{post.text}</p>
         </div>
         <div className='post__buttons'>
-            <button className='post__button' onClick={() => deletePost(post)}>Удалить</button>
+            <button 
+              className='post__button' 
+              onClick={handleDelete}
+              disabled={isDeleting}
+            >
+              Удалить
+            </button>
         </div>
     </li>
   )
